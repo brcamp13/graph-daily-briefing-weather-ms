1037437f89f605f7c87172a99dbc7bd3dc293ab0
Brandon Campbell <brandon.camp.bell@outlook.com>
2025-05-20 13:07:30 -0500

    Initial commit

---
+++ src/main/java/com/bmcapps/graphdailybriefingweather/GraphdailybriefingweatherApplication.java
@@ -0,0 +1,13 @@
+package com.bmcapps.graphdailybriefingweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphdailybriefingweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphdailybriefingweatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmcapps/graphdailybriefingweather/GraphdailybriefingweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmcapps.graphdailybriefingweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphdailybriefingweatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
